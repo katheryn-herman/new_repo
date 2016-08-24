4a20e396d7332bdaf12cd1ff9e0cfc34ba6e91ca
katheryn-herman <katheryn.herman@hitachiconsulting.com>
2016-08-24 14:13:18 -0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,14 @@
+<html>
+   <body>
+   
+      <script language="javascript" type="text/javascript">
+         <!--
+            document.write("Hello World!")
+		alert("Katheryn");
+         //-->
+      </script>
+
+
+      
+   </body>
+</html>
